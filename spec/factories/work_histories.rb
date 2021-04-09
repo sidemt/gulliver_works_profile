3a0d662dfa1d8dc4b337f02a5867911984cd4776
sidemt <25644062+sidemt@users.noreply.github.com>
2021-04-09 07:18:27 +0000

    Add tests for WorkHistoryController

--- spec/factories/work_histories.rb
+++ spec/factories/work_histories.rb
@@ -1,14 +1,18 @@
 FactoryBot.define do
   factory :work_history do
-    is_employed { false }
-    occupation { nil }
-    industry { nil }
-    position { "MyString" }
-    annual_income { 1 }
-    management_experience { 1 }
-    job_summary { "MyString" }
-    since_date { "2021-04-08" }
-    until_date { "2021-04-08" }
-    name { "MyString" }
+    is_employed { true }
+    position { "主任" }
+    annual_income { 4000000 }
+    management_experience { 3 }
+    job_summary { "Ruby on Railsを使ったバックエンド開発" }
+    since_date { "2019-08-01" }
+    until_date { "2020-05-01" }
+    sequence(:name) { |n| "社名_#{n}" }
+    association :occupation
+    association :industry
+
+    trait :is_employed_false do
+      is_employed { false }
+    end
   end
 end
