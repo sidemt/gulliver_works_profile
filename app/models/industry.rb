412ef31c84b3bf0e4c4e7878134b7444afa9bd3e
sidemt <25644062+sidemt@users.noreply.github.com>
2021-04-10 04:42:52 +0000

    Change has_many dependent option to nullify

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -2,7 +2,7 @@
 # Industry
 class Industry < ApplicationRecord
   belongs_to :industry_category
-  has_many :work_histories, dependent: :destroy
+  has_many :work_histories, dependent: :nullify
 
   validates :name, presence: true
   validates :industry_category_id, uniqueness: { scope: :name }
--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -2,7 +2,7 @@
 # Occupation
 class Occupation < ApplicationRecord
   belongs_to :occupation_sub_category
-  has_many :work_histories, dependent: :destroy
+  has_many :work_histories, dependent: :nullify
 
   validates :name, presence: true, uniqueness: true
 end
